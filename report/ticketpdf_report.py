b22df9f1e36614a817e265ac8f1ec33606bb979c
Simplify-ERP® <github@euroblaze.de>
2023-04-15 12:15:29 +0200

    Create ticketpdf_report.py

---
+++ report/ticketpdf_report.py
@@ -0,0 +1,16 @@
+from odoo import models
+
+class TicketPdfReport(models.AbstractModel):
+    _name = "report.ticketpdf.report_ticketpdf_document"
+    _description = "TicketPDF Report"
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        # Get necessary data and pass it to the report template
+        tickets = self.env['helpdesk.ticket'].browse(docids)
+        return {
+            'doc_ids': tickets.ids,
+            'doc_model': 'helpdesk.ticket',
+            'data': data,
+            'docs': tickets,
+        }
