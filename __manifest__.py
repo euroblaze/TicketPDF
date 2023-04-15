6bc3cf87346c3e5d26dbd6ba37ae501660b4eb41
Simplify-ERP® <github@euroblaze.de>
2023-04-15 12:13:55 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "TicketPDF",
+    "version": "1.0",
+    "author": "Your Name",
+    "category": "Tools",
+    "website": "https://yourwebsite.com",
+    "license": "AGPL-3",
+    "summary": "Generate a PDF from a helpdesk ticket description",
+    "depends": ["base", "helpdesk"],
+    "data": [
+        "report/ticketpdf_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
